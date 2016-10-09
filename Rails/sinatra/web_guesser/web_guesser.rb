d36ea2117c5363f105b9eabc3f0feb15d24ad470
Dylan <nielsen.dylan@gmail.com>
2016-10-08 21:38:10 -0700

    I3 finished

--- Rails/sinatra/web_guesser/web_guesser.rb
+++ Rails/sinatra/web_guesser/web_guesser.rb
@@ -1,6 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
-number = rand(100)
-get '/' do  
-  erb :index, :locals => {:number => number}
+set :secret, rand(100)
+get '/' do 
+  guess = params['guess'].to_i 
+  message = check_guess(guess)
+  erb :index, :locals => {:number => settings.secret, :message => message}
+end
+
+def check_guess(guess)
+  if guess < settings.secret - 5 
+         "#{guess} is way too low!" 
+      elsif guess > settings.secret + 5 
+         "#{guess} is way too high!" 
+      elsif guess > settings.secret 
+         "#{guess} is too high!" 
+      elsif guess < settings.secret 
+         "#{guess} is too low!"       
+      elsif guess == settings.secret 
+         "That's Right! \n\nThe SECRET NUMBER is #{settings.secret} "
+      end 
 end
