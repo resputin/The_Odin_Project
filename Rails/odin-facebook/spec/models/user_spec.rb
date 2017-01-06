1e27900d17090b6e927a040ee1d0b77bdb722ba8
Dylan <nielsen.dylan@gmail.com>
2017-01-06 14:14:53 -0800

    user validations add factory girl

--- Rails/odin-facebook/spec/models/user_spec.rb
+++ Rails/odin-facebook/spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe User do
+    before(:each) do
+      @user_attributes = FactoryGirl.attributes_for(:user)
+    end
+
+    it "should create a new instance of a user given valid attributes" do
+      User.create!(@user_attributes)
+    end
+  end
 end
